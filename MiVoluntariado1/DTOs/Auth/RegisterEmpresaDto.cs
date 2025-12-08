ca8df58375baae73dc8c1795a8c04b9a82e51124
Mikel Ribera <a2023115604@estudiantes.upsa.edu.bo>
2025-12-08 19:24:55 -0400

    auth completo

--- MiVoluntariado1/DTOs/Auth/RegisterEmpresaDto.cs
+++ MiVoluntariado1/DTOs/Auth/RegisterEmpresaDto.cs
@@ -1,6 +1,13 @@
-﻿namespace Connecta.DTOs.Auth;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class RegisterEmpresaDto
+namespace MiVoluntariadoAPI.DTOs.Auth
 {
-    
+    public class RegisterEmpresaDto
+    {
+        [Required] public required string Nombre { get; set; }
+        [Required] [EmailAddress] public required string Email { get; set; }
+        [Required] [MinLength(6)] public required string Password { get; set; }
+        public string? Direccion { get; set; }
+        public string? Descripcion { get; set; }
+    }
 }
--- MiVoluntariado1/DTOs/Auth/RegisterVoluntarioDto.cs
+++ MiVoluntariado1/DTOs/Auth/RegisterVoluntarioDto.cs
@@ -1,6 +1,12 @@
-﻿namespace Connecta.DTOs.Auth;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class RegisterVoluntarioDto
+namespace MiVoluntariadoAPI.DTOs.Auth
 {
-    
+    public class RegisterVoluntarioDto
+    {
+        [Required] public required string Nombre { get; set; }
+        [Required] public required string Apellido { get; set; }
+        [Required] [EmailAddress] public required string Email { get; set; }
+        [Required] [MinLength(6)] public required string Password { get; set; }
+    }
 }
